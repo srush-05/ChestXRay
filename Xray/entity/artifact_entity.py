c390bd51da4115974a07e3e1f76f7a4b7e6d1c3e
Mohneet Kaur <mohneetkaursandhu@example.com>
2024-09-25 15:56:17 -0500

    training config updated

--- Xray/entity/artifact_entity.py
+++ Xray/entity/artifact_entity.py
@@ -1,1 +1,10 @@
+from dataclasses import dataclass
+from torch.utils.data.dataloaders import DataLoader
 
+
+@dataclass
+class DataIngestionArtifact:
+    train_file_path: str
+    test_file_path: str
+
+    
